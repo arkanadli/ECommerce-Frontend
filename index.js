a0046c4839fea03d22754609db104cfccf1f5a26
arkanadli <uzzomaki@gmail.com>
2025-04-26 13:06:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -13,16 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 const backendUrl = 'http://[ip-private-backend]:3000';
 
 app.get('/', async (req, res) => {
-  try {
-    // Dapatkan produk dari backend
-    const response = await axios.get(`${backendUrl}/products`);
-    res.render('index', { products: response.data });
-  } catch (error) {
-    console.error('Error mengambil produk:', error);
-    res.status(500).send('Error mengambil produk');
-  }
+    try {
+        // Dapatkan produk dari backend
+        const response = await axios.get(`${backendUrl}/products`);
+        res.render('index', { products: response.data });
+    } catch (error) {
+        console.error('Error mengambil produk:', error);
+        res.status(500).send('Error mengambil produk');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Aplikasi frontend berjalan di http://localhost:${port}`);
+    console.log(`Aplikasi frontend berjalan di http://localhost:${port}`);
 });
