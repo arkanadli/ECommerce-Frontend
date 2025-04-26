7662eb7eae40195cbc3a7c16f00890d9b80e3586
arkanadli <uzzomaki@gmail.com>
2025-04-26 11:02:04 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+const port = 80;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+// Endpoint API backend
+const backendUrl = 'http://[ip-private-backend]:3000';
+
+app.get('/', async (req, res) => {
+  try {
+    // Dapatkan produk dari backend
+    const response = await axios.get(`${backendUrl}/products`);
+    res.render('index', { products: response.data });
+  } catch (error) {
+    console.error('Error mengambil produk:', error);
+    res.status(500).send('Error mengambil produk');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Aplikasi frontend berjalan di http://localhost:${port}`);
+});
